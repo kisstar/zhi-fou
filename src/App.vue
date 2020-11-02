1e9182ab7f542b072ff1b5b0bf4fc2f9c616bf7f
kisstar <dwh.chn@foxmail.com>
2020-11-02 17:36:00 +0800

    feat: 构建 ColumnList 组件基础

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <template>
-  <div class="container">Hello World!</div>
+  <div class="container">
+    <column-list :list="list"></column-list>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import ColumnList, { ColumnProps } from "./components/ColumnList.vue";
+
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: "test1的标题",
+    avatar: "http://www.dmoe.cc/random.php",
+    description: "这是用来测试的专栏，有一段非常有意思的简介，欢迎阅读和编辑哦"
+  }
+];
 
 export default defineComponent({
-  name: "App"
+  name: "App",
+  components: {
+    ColumnList
+  },
+  setup() {
+    return {
+      list: testData
+    };
+  }
 });
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/ColumnList.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul>
+    <li v-for="column in list" :key="column.id">
+      <img :src="column.avatar" :alt="column.title" width="50" height="50" />
+      <h5 v-text="column.title"></h5>
+      <p v-text="column.description"></p>
+      <a href="#">进入专栏</a>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+export interface ColumnProps {
+  id: number;
+  title: string;
+  avatar: string;
+  description: string;
+}
+
+export default defineComponent({
+  name: "ColumnList",
+  props: {
+    list: {
+      type: Array as PropType<ColumnProps[]>,
+      required: true
+    }
+  }
+});
+</script>
+
+<style lang="scss"></style>
