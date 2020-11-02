814b4cd0d5bd62535091278ad9000fcfbb6eea04
kisstar <dwh.chn@foxmail.com>
2020-11-02 14:44:23 +0800

    feat: 支持 SASS 并引入 Bootstrap 作为样式库

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="container">Hello World!</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld
-  }
+  name: "App"
 });
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
