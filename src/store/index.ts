790d3c76da88c01400d9d24bbe8b45e4c27c13fb
kisstar <dwh.chn@foxmail.com>
2021-09-17 00:09:01 +0800

    fix: 刷新后丢失登录态

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,15 +3,17 @@ import mutations from "./mutations";
 import actions from "./actions";
 import { AppState } from "@/types/interface";
 
+const token = localStorage.getItem("token") || "";
+
 const store = createStore<AppState>({
   state: {
     // 登录凭证
-    token: "",
+    token,
     // 当前是否有接口在加载
     loading: false,
     // 用户信息
     user: {
-      isLogin: false
+      isLogin: !!token
     },
     columnList: [], // 专栏列表
     currentColumn: null, // 当前显示的专栏
